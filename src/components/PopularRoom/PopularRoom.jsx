58fe8ac782da4aad79ef76f5bc3fdf5a23199642
sajib689 <jibonraja11@gmail.com>
2024-08-24 12:45:44 +0600

    fetch data from popular_room json data

--- src/components/PopularRoom/PopularRoom.jsx
+++ src/components/PopularRoom/PopularRoom.jsx
@@ -1,11 +1,31 @@
-
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const PopularRoom = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    const [popularRooms, setPopularRooms] = useState([])
+    useEffect( () => {
+        axios.get('popular_room.json')
+        .then( data => {
+            setPopularRooms(data.data)
+        })
+        .catch(err => {
+            console.log(err.message);
+        })
+    },[])
+  return (
+    <div className=" mt-24 mb-24 lg:mb-48 md:mb-48">
+        {/* title and description */}
+      <div className="text-center">
+        <h1 className="text-4xl font-bold">Most Popular Rooms </h1>
+        <p className="mt-5 m-2">
+          Explore our most popular rooms, chosen for their comfort, style, and
+          amenities. <br /> Discover why guests love staying in these well-appointed
+          spaces!
+        </p>
+      </div>
+
+    </div>
+  );
 };
 
 export default PopularRoom;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,8 +7,8 @@ const Home = () => {
   return <div>
     <Banner></Banner>
     <Property></Property>
-    <Review/>
     <PopularRoom/>
+    <Review/>
   </div>;
 };
 
