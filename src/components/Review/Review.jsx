3bcda466f19f5c4cd35d8a361cf3d5fa0c6fd875
sajib689 <jibonraja11@gmail.com>
2024-08-24 11:56:05 +0600

    add review silder using by swiper js

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,11 +1,58 @@
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay } from "swiper/modules";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import ReviewCard from "../common/ReviewCard";
 
 const Review = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("review.json")
+      .then((data) => {
+        setReviews(data.data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
+  return (
+    <div className="mt-48 mb-80">
+      <Swiper
+        autoplay={{
+          delay: 1500,
+          disableOnInteraction: false,
+        }}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination, Autoplay]}
+        className="mySwiper"
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+          },
+         
+          768: {
+            slidesPerView: 2, 
+          },
+        
+          1024: {
+            slidesPerView: 3, 
+          },
+        }}
+      >
+        {reviews.map((review) => (
+          <SwiperSlide key={review.name}>
+            <ReviewCard review={review} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 };
 
 export default Review;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../../components/banner/Banner";
 import Property from "../../components/property/Property";
+import Review from "../../components/Review/Review";
 
 const Home = () => {
   return <div>
     <Banner></Banner>
     <Property></Property>
+    <Review/>
   </div>;
 };
 
