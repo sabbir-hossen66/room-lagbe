b03ea491b104ea8b443930673bb72a9dbf593d9b
Sabbir Hossen <sabbir.ms2000@gmail.com>
2024-08-24 15:01:52 +0600

    searchfiled added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,13 +2,15 @@ import Banner from "../../components/banner/Banner";
 import PopularRoom from "../../components/PopularRoom/PopularRoom";
 import Property from "../../components/property/Property";
 import Review from "../../components/Review/Review";
+import SearchField from "../../components/SearchField/SearchField";
 
 const Home = () => {
   return <div>
     <Banner></Banner>
+    <SearchField></SearchField>
     <Property></Property>
-    <PopularRoom/>
-    <Review/>
+    <PopularRoom />
+    <Review />
   </div>;
 };
 
